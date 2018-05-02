08571d271be91f9ab28bca505e4e2eaef4d7b88f
wanglei01 <wanglei01@alibaba-inc.com>
2018-05-02 17:07:02 +0800

    fix

--- loggo.go
+++ loggo.go
@@ -4,27 +4,9 @@ import ()
 
 var (
 	default_logger Logger
+
+	DefFormat = "%{time:01-02 15:04:05.9999} #%{pid}.%{gid} %{shortpkg}/%{shortfile}/%{callpath:3} %{color:bold}%{level:.4s}%{color:reset} %{message}"
 )
-
-type loggo struct {
-	//backend Backend
-}
-
-func (log *loggo) Log() {
-	//log.backend.Log()
-}
-
-//func SetBackend(be ...Backend) {
-//	if len(be) == 1 {
-//
-//	} else {
-//
-//	}
-//	return
-//}
-//func Close() {
-//	default_logger
-//}
 
 func Debug(args ...interface{}) {
 	default_logger.log(DEBUG, 1, nil, args...)
@@ -57,20 +39,11 @@ func Fatalf(format string, args ...interface{}) {
 	default_logger.log(FATAL, 1, &format, args...)
 }
 
-//func SetFileWriter(filename string) {
-//	default_logger.SetFileWriter(filename)
-//}
-//func SetFormatter(format string) {
-//	default_logger.SetFormatter(format)
-//}
-//func SetLevel(level Flag) {
-//	default_logger.SetLevel(level)
-//}
 func SetDefaultLogger(logger Logger) {
 	default_logger = logger
 }
 
 func init() {
-	default_logger = NewLevelLogger(INFO, "%{time:01-02 15:04:05.9999} #%{pid}.%{gid} %{shortpkg}/%{shortfile}/%{callpath:3} %{color:bold}%{level:.4s}%{color:reset} %{message}")
+	default_logger = NewLevelLogger(INFO, DefFormat)
 	//default_logger = NewFileLevelLogger(INFO,"debug.log", "%{time:01-02 15:04:05.9999} #%{pid}.%{gid} %{shortpkg}/%{shortfile}/%{callpath:3} %{color:bold}%{level:.4s}%{color:reset} %{message}")
 }
